68bd847f94e6907e314bbdb2ff48d1e60863f76f
Felipe Basilio <felipe.basilio@avenuecode.com>
2023-04-03 08:23:53 -0300

    Iniciando com o React

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./Post"
+
+function App() {
+  return (
+    <div>
+      <Post 
+        author="Felipe Basilio"
+        content="teste1"
+      />
+      <Post 
+        author="Felipe Basilio"
+        content="teste2"
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+    return (
+        <div>
+            <p>{props.author}</p>
+            <strong>{props.content}</strong>
+        </div> 
+    )
+}
